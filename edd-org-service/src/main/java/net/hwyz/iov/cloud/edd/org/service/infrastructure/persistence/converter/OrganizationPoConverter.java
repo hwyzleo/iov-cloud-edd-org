dd96f61a4bb9702a3130532a3698f0e603f83aac
hwyz_leo <hwyz.leo.ye@gmail.com>
2026-05-14 17:28:06 +0800

    feat: create OrganizationPoConverter for PO-Domain conversion

---
+++ edd-org-service/src/main/java/net/hwyz/iov/cloud/edd/org/service/infrastructure/persistence/converter/OrganizationPoConverter.java
@@ -0,0 +1,28 @@
+package net.hwyz.iov.cloud.edd.org.service.infrastructure.persistence.converter;
+
+import net.hwyz.iov.cloud.edd.org.service.domain.model.aggregate.Organization;
+import net.hwyz.iov.cloud.edd.org.service.domain.model.valueobject.OrganizationCode;
+import net.hwyz.iov.cloud.edd.org.service.infrastructure.persistence.po.OrgPo;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface OrganizationPoConverter {
+    
+    Organization toDomain(OrgPo orgPo);
+    
+    OrgPo toPo(Organization organization);
+    
+    List<Organization> toDomainList(List<OrgPo> orgPoList);
+    
+    List<OrgPo> toPoList(List<Organization> organizations);
+    
+    default String map(OrganizationCode code) {
+        return code != null ? code.getValue() : null;
+    }
+    
+    default OrganizationCode map(String code) {
+        return code != null ? new OrganizationCode(code) : null;
+    }
+}
