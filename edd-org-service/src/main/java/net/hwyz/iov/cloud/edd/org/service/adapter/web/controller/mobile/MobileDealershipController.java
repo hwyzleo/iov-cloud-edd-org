0a9ac34e7e63065b75c77dc0da8ba0ad38aabfe6
hwyz_leo <hwyz.leo.ye@gmail.com>
2026-05-15 20:47:18 +0800

    重构: 调整手机端门店接口

--- edd-org-service/src/main/java/net/hwyz/iov/cloud/edd/org/service/adapter/web/controller/mobile/MobileDealershipController.java
+++ edd-org-service/src/main/java/net/hwyz/iov/cloud/edd/org/service/adapter/web/controller/mobile/MobileDealershipController.java
@@ -6,9 +6,12 @@ import net.hwyz.iov.cloud.edd.org.api.enums.DealershipServiceType;
 import net.hwyz.iov.cloud.edd.org.api.vo.Dealership;
 import net.hwyz.iov.cloud.edd.org.service.application.service.DealershipAppService;
 import net.hwyz.iov.cloud.framework.common.bean.ApiResponse;
-import net.hwyz.iov.cloud.framework.common.bean.ClientAccount;
 import net.hwyz.iov.cloud.framework.common.util.ParamHelper;
-import org.springframework.web.bind.annotation.*;
+import net.hwyz.iov.cloud.framework.web.controller.BaseController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -21,20 +24,19 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/mobile/dealership/v1")
-public class MobileDealershipController {
+public class MobileDealershipController extends BaseController {
 
     private final DealershipAppService dealershipAppService;
 
     /**
      * 获取销售门店列表
      *
-     * @param serviceType   服务类型
-     * @param provinceCode  省级行政区代码
-     * @param cityCode      地区级行政区代码
-     * @param countyCode    县级行政区代码
-     * @param requestLon    请求经度
-     * @param requestLat    请求纬度
-     * @param clientAccount 终端用户
+     * @param serviceType  服务类型
+     * @param provinceCode 省级行政区代码
+     * @param cityCode     地区级行政区代码
+     * @param countyCode   县级行政区代码
+     * @param requestLon   请求经度
+     * @param requestLat   请求纬度
      * @return 销售门店列表
      */
     @GetMapping("")
@@ -43,9 +45,8 @@ public class MobileDealershipController {
                                                            @RequestParam(required = false) String cityCode,
                                                            @RequestParam(required = false) String countyCode,
                                                            @RequestParam(required = false) String requestLon,
-                                                           @RequestParam(required = false) String requestLat,
-                                                           @RequestHeader ClientAccount clientAccount) {
-        log.info("手机客户端[{}]获取服务类型[{}]销售门店列表", ParamHelper.getClientAccountInfo(clientAccount), serviceType);
+                                                           @RequestParam(required = false) String requestLat) {
+        log.info("手机客户端[{}]获取服务类型[{}]销售门店列表", ParamHelper.getClientAccountInfo(), serviceType);
         return ApiResponse.ok(dealershipAppService.getDealershipList(serviceType, provinceCode, cityCode, countyCode, requestLon, requestLat));
     }
 
